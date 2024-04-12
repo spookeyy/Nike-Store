94999e06ae0d28047d59563f587c9227799341de
spookeyy <pangasmeshack@gmail.com>
2024-04-13 00:00:50 +0300

    update server

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
-const server = require("server");
-const { get, post } = server.router;
-const bodyParser = require("body-parser");
-// Launch server with options and routes
-server({ port: 8080 }, [
-  get("/", (ctx) => "Hello world"),
-  post("/", bodyParser.json(), (ctx) => {
-    console.log(ctx.data);
-    const { data } = ctx.request.body;
-    let categories;
-    try {
-      categories = JSON.parse(data);
-      N;
-    } catch (error) {
-      console.error("Error parsing data:", error);
-      return (ctx.response.status = 400); // Set status code for bad request
-    }
+const http = require('http');
+const app = require('./server');
+const { hostname } = require('os');
+const { Console } = require('console');
 
-    function addtocart(a, categories) {
-      if (categories) {
-        cart.push({ ...categories[a] });
-        displayCart();
-      } else {
-        console.error("Categories not available yet");
-      }
-    }
-    addtocart();
+const port = process.env.PORT || 5000;
+const server = http.createServer(app)
 
-    return "Success";
-  }),
-]);
+server.listen(port, hostname , () => {
+  console.log(`started on port ${port}`);
+});
